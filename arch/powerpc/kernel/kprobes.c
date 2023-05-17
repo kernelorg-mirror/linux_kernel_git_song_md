d4dbd22faf1717ce798327b60fc98cd5b7628ec8
Song Liu <song@kernel.org>
2023-05-17 15:28:10 -0700

    powerpc/module: use module_alloc_type
    Feed parameters used by module_alloc to module_alloc_type().
    
    Also updated kprobes call of module_alloc with module_alloc_type.
    
    No functional change intended.
    
    Signed-off-by: Song Liu <song@kernel.org>
    
    ---
    I tried to cross compile for ppc64, but the following command failed before
    this change.
    
    make ARCH=powerpc LLVM=1 CC=clang CROSS_COMPILE=ppc64-linux-gnu- -j
    [...]
      VDSO64L arch/powerpc/kernel/vdso/vdso64.so.dbg
    arch/powerpc/kernel/vdso/vdso64.so.dbg: dynamic relocations are not
    supported
    make[1]: *** [arch/powerpc/kernel/vdso/Makefile:78:
    arch/powerpc/kernel/vdso/vdso64.so.dbg] Error 1
    make: *** [arch/powerpc/Makefile:378: vdso_prepare] Error 2

--- arch/powerpc/kernel/kprobes.c
+++ arch/powerpc/kernel/kprobes.c
@@ -130,7 +130,7 @@ void *alloc_insn_page(void)
 {
 	void *page;
 
-	page = module_alloc(PAGE_SIZE);
+	page = module_alloc_type(PAGE_SIZE, MOD_TEXT);
 	if (!page)
 		return NULL;
 
--- arch/powerpc/kernel/module.c
+++ arch/powerpc/kernel/module.c
@@ -89,24 +89,20 @@ int module_finalize(const Elf_Ehdr *hdr,
 	return 0;
 }
 
-static __always_inline void *
-__module_alloc(unsigned long size, unsigned long start, unsigned long end, bool nowarn)
+static struct mod_type_allocator powerpc_mod_type_allocator = {
+	.params = {
+		.granularity	= PAGE_SIZE,
+		.alignment	= 1,
+	},
+};
+
+void __init module_alloc_type_init(struct mod_allocators *allocators)
 {
 	pgprot_t prot = strict_module_rwx_enabled() ? PAGE_KERNEL : PAGE_KERNEL_EXEC;
-	gfp_t gfp = GFP_KERNEL | (nowarn ? __GFP_NOWARN : 0);
+	struct mod_alloc_params *params = &powerpc_mod_type_allocator.params;
+	struct vmalloc_params *vmp = &params->vmp[0];
+	int i;
 
-	/*
-	 * Don't do huge page allocations for modules yet until more testing
-	 * is done. STRICT_MODULE_RWX may require extra work to support this
-	 * too.
-	 */
-	return __vmalloc_node_range(size, 1, start, end, gfp, prot,
-				    VM_FLUSH_RESET_PERMS,
-				    NUMA_NO_NODE, __builtin_return_address(0));
-}
-
-void *module_alloc(unsigned long size)
-{
 #ifdef MODULES_VADDR
 	unsigned long limit = (unsigned long)_etext - SZ_32M;
 	void *ptr = NULL;
@@ -114,14 +110,29 @@ void *module_alloc(unsigned long size)
 	BUILD_BUG_ON(TASK_SIZE > MODULES_VADDR);
 
 	/* First try within 32M limit from _etext to avoid branch trampolines */
-	if (MODULES_VADDR < PAGE_OFFSET && MODULES_END > limit)
-		ptr = __module_alloc(size, limit, MODULES_END, true);
 
-	if (!ptr)
-		ptr = __module_alloc(size, MODULES_VADDR, MODULES_END, false);
+	if (MODULES_VADDR < PAGE_OFFSET && MODULES_END > limit) {
+		vmp->start = limit;
+		vmp->end = MODULES_END;
+		vmp->gfp_mask = GFP_KERNEL | __GFP_NOWARN;
+		vmp->pgprot = prot;
+		vmp->vm_flags = VM_FLUSH_RESET_PERMS;
+		vmp = &params->vmp[1];
+	}
 
-	return ptr;
+	vmp->start = MODULES_VADDR;
+	vmp->end = MODULES_END;
+	vmp->gfp_mask = GFP_KERNEL;
+	vmp->pgprot = prot;
+	vmp->vm_flags = VM_FLUSH_RESET_PERMS;
 #else
-	return __module_alloc(size, VMALLOC_START, VMALLOC_END, false);
+	vmp->start = VMALLOC_START;
+	vmp->end = VMALLOC_END;
+	vmp->gfp_mask = GFP_KERNEL;
+	vmp->pgprot = prot;
+	vmp->vm_flags = VM_FLUSH_RESET_PERMS;
 #endif
+
+	for (i = 0; i < MOD_MEM_NUM_TYPES; i++)
+		allocators->types[i] = &powerpc_mod_type_allocator;
 }
