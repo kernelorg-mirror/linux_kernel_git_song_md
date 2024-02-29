039567df2362ce8bbe1d41849611a194f054c43b
Xiao Ni <xni@redhat.com>
2024-02-29 23:49:40 +0800

    md: Set MD_RECOVERY_FROZEN before stop sync thread
    After patch commit f52f5c71f3d4b ("md: fix stopping sync thread"), dmraid
    stops sync thread asynchronously. The calling process is:
    dev_remove->dm_destroy->__dm_destroy->raid_postsuspend->raid_dtr
    
    raid_postsuspend does two jobs. First, it stops sync thread. Then it
    suspend array. Now it can stop sync thread successfully. But it doesn't
    set MD_RECOVERY_FROZEN. It's introduced by patch f52f5c71f3d4b. So after
    raid_postsuspend, the sync thread starts again. raid_dtr can't stop the
    sync thread because the array is already suspended.
    
    This can be reproduced easily by those commands:
    while [ 1 ]; do
    vgcreate test_vg /dev/loop0 /dev/loop1
    lvcreate --type raid1 -L 400M -m 1 -n test_lv test_vg
    lvchange -an test_vg
    vgremove test_vg -ff
    done
    
    Fixes: f52f5c71f3d4 ("md: fix stopping sync thread")
    Signed-off-by: Xiao Ni <xni@redhat.com>
    Signed-off-by: Song Liu <song@kernel.org>
    Link: https://lore.kernel.org/r/20240229154941.99557-6-xni@redhat.com

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -6374,6 +6374,7 @@ static void __md_stop_writes(struct mddev *mddev)
 void md_stop_writes(struct mddev *mddev)
 {
 	mddev_lock_nointr(mddev);
+	set_bit(MD_RECOVERY_FROZEN, &mddev->recovery);
 	__md_stop_writes(mddev);
 	mddev_unlock(mddev);
 }
